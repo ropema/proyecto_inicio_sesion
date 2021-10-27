9f6e664eeec5466e860b0202548d8c2f75d7b027
ropema <ropema@misena.edu.co>
2021-10-26 22:27:00 -0300

    ingreso de registros a la base y sentencias de validacion en el modelo

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,7 @@
 class Usuario < ApplicationRecord
+    validates :first_name, :last_name, :email_address, :age, presence: true
+    validates :age, numericality: true
+    validates :first_name, :last_name, length: { minimum: 2 }
+    validates :age, numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 150 }
+
 end
